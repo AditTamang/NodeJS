2f2d145348992903091f43c78008f9970ddc960f
AditTamang <tamangadit86@gmail.com>
2024-07-22 15:02:56 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+console.log("Bye World....!!!")
+
+var bottle = "water"
+var age = 22
+var isNepali = true
+
+console.log(bottle)
+console.log(age)
+console.log(isNepali)
+
+let name = "Adit"
+console.log("My name is", name)
+
+let name1 = "Adit"
+let name2 = "Tamang"
+let name3 ="Pakhrin"
+let name4 = "Itahari"
+let name5 = "Kathmandu"
+
+let friends = ["Adit ", "Sandhya", "MANISHa" ,"Verish" , "Shristi"]
+console.log(friends[0])
+console.log(friends.length)
+
+// console.log(name1)
+// console.log(name2)
+// console.log(name3)
+// console.log(name4)
+// console.log(name5)
+
+var me = [{
+    name: "Adit",
+    age :21 , 
+    location : "Tarahara"
+},
+{
+    name: "Tamang",
+    age: 22
+}]
+console.log(me.age)
+
+console.log(me[0].name,me[1].age)
+
+var me2 = [{
+    name:"sANDHYA",
+    age : 22
+},
+{
+    names : ["Hero","Gunda","Pappu"],
+    age: 20
+}]
+
+console.log(me2[1].names[0])
+
+var data = {
+    data : [
+        {
+            name : ["Adit", "Don","Tamang"],
+            age: 20
+        }
+    ]
+}
+
+console.log(data.data[0].name[1])
